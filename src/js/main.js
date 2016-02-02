be12db5edd4f682e6b6bd77aa4182643ea2d95e6
Carl Vereen <carlvereen@gmail.com>
2016-02-02 15:12:49 -0700

    added guest name capitalization and guest email validator

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,12 @@
 
 // Date Picker functions Bootstrap
 $(function () {
-    $('.datetimepickerDate').datepicker();
-    // $('#datetimepicker3').datetimepicker({
-    //   pickDate: false
-    // });
-    // $('#datetimepicker4').datetimepicker({
-    //   pickDate: false
-    // });
-
+    $('#datetimepicker1').datepicker({
+        autoclose: true
+    });
+    $('#datetimepicker2').datepicker({
+        autoclose: true
+    });
 
 });
 
@@ -81,6 +79,7 @@ $(document).ready(function(){
 
 function MyViewModel() {
     var self = this;
+    //Event form variables
     self.eventForm = ko.observable(true);
     self.eventType = ko.observable('');
     self.eventName = ko.observable('');
@@ -95,19 +94,23 @@ function MyViewModel() {
     self.datevisible = ko.observable(false);
     self.eventMessage = ko.observable('');
     self.addEndButton = ko.observable('Add End Date');
+    self.dateButtonClass = ko.observable('btn btn-default');
+    //Profile form variables
     self.personalForm = ko.observable(false);
     self.moreDetails = ko.observable(false);
-    self.guestButton = ko.observable('Create Guest List');
-    self.guestList = ko.observable(false);
-    self.guestName = ko.observable('');
-    self.guestEmail = ko.observable('');
-    self.guestListArray = ko.observableArray('');
-    self.dateButtonClass = ko.observable('btn btn-default');
     self.detailButtonText = ko.observable('Add Detail Profile');
     self.detailButtonClass = ko.observable('btn btn-default');
-
-
+    //Guest List variables
+    self.guestButton = ko.observable('Create Guest List');
+    self.guestList = ko.observable(false);
+    self.guestNameToAdd = ko.observable('');
+    self.guestEmailToAdd = ko.observable('');
+    self.guestListArray = ko.observableArray('');
+    var idNum = 1001;
     
+
+
+    //Print to screen for review of event details
     self.printEventName = ko.computed(function() {
         return self.eventName();
     });
@@ -124,29 +127,129 @@ function MyViewModel() {
         return self.eventLocationAddress();
     });
     self.printEventDates = ko.computed(function() {
-        return 'Start Date: ' + self.eventStartDate() + ' at ' + self.startTime() + ' to End Date: ' + self.eventEndDate() + ' at ' + self.endTime();
+        if(self.eventStartDate() === '' ) {
+            return; 
+        
+        }else{
+            if(self.startTime() === '') {
+                if(self.eventEndDate() === '') {
+                    return 'Start Date: ' + self.eventStartDate();
+                }else{
+                    if(self.endTime() === '') {
+                         return 'Start Date: ' + self.eventStartDate() + ' to ' + self.eventEndDate();
+                    }else{
+                        return 'Start Date: ' + self.eventStartDate() + ' to ' + self.eventEndDate() + ' at ' + self.endTime();
+                    }
+                }
+            }else{
+               if(self.eventEndDate() === '') {
+                    return 'Start Date: ' + self.eventStartDate() + ' at ' + self.startTime();
+                }else{
+                    if(self.endTime() === '') {
+                         return 'Start Date: ' + self.eventStartDate() + ' at ' + self.startTime() + ' to ' + self.eventEndDate();
+                    }else{
+                        return 'Start Date: ' + self.eventStartDate() + ' at ' + self.startTime() + ' to ' + self.eventEndDate() + ' at ' + self.endTime();
+                    }
+                }
+            }
+        }
     });
     self.printEventMessage = ko.computed(function() {
         return self.eventMessage();
     });
     self.printEventType = ko.computed(function() {
+        
         return self.eventType();
     });
+
+    //Add end date if requested
     self.showEndDate = function() {
         if(self.datevisible()) {
             self.addEndButton('Add End Date');
             self.datevisible(false);
             self.dateButtonClass('btn btn-default');
+            self.eventEndDate('');
+            self.endTime('');
         }else{
             self.datevisible(true);
             self.addEndButton('Remove End Date');
             self.dateButtonClass('btn btn-danger');
         }
     };
+
+
+    //show the guest list table and form options 
+    self.showGuestList = function() {
+        if(self.guestList()) {
+            self.guestList(false);
+            self.guestButton('Create Guest List');
+        }else{
+            self.guestList(true);
+            self.guestButton('Hide Guest List');
+        }
+    };
+    //Add guest to list and print to screen
+    self.capName = ko.computed(function() {
+      var guestNameArray = [];
+      var searchString = /\s/g;
+      var subSpace = self.guestNameToAdd().toLowerCase().split(searchString);
+      
+      function lastName () {
+        for (i=0; i<subSpace.length; i++) {
+          var nameVar = subSpace[i];
+          var nameSubFirst = nameVar.substr(0,1).toUpperCase();
+          var nameSubLast = nameVar.substr(1);
+          var nameResult = nameSubFirst + nameSubLast;
+          guestNameArray.push(nameResult);       
+        }
+       
+      };
+      var callfunction = lastName();
+      var subLast = guestNameArray.join(" ");
+      
+      return subLast;
+       
+    }, self);
+
+    self.validEmail = ko.computed(function() {
+      var emailGuest = self.guestEmailToAdd();
+      var containsAt = emailGuest.search(/[a-zA-Z]@[a-zA-z]/g) > -1;
+      var indexPeriod = emailGuest.search(/\./g);
+      var isWeb = emailGuest.substr(indexPeriod);
+      var containsPeriod = 3 < isWeb.length < 5;
+      function validationCheck () {
+                          if(containsAt === containsPeriod) {
+                            return emailGuest;
+                          }else{
+                            return 'Invalid Email';
+                          }
+                        };
+      var emailCheck = validationCheck();
+      return emailCheck;
+    });
+    self.addGuest = function() {
+        if (self.guestNameToAdd() != '') {
+            idNum += 1;
+
+            var nameGuest = self.capName();
+            var emailGuest = self.validEmail();
+            self.guestListArray.push({guestName: nameGuest, guestEmail: emailGuest, guestID: idNum}); 
+            self.guestNameToAdd('');
+            self.guestEmailToAdd('');
+        }
+    }.bind(self); 
+
+    self.removeGuest = function(guest) {
+        self.guestListArray.remove(guest);
+    };
+
+    //Request personal information after event form is saved
     self.showPersonal = function() {
         self.eventForm(false);
         self.personalForm(true);
     };
+
+    //Allow for greater profile details if desired
     self.showMoreDetails = function() {
         if(self.moreDetails()) {
             self.moreDetails(false);
@@ -158,19 +261,23 @@ function MyViewModel() {
             self.detailButtonClass('btn btn-danger');
         }
     };
-    self.showGuestList = function() {
-        if(self.guestList()) {
-            self.guestList(false);
-            self.guestButton('Create Guest List');
-        }else{
-            self.guestList(true);
-            self.guestButton('Remove Guest List');
-        }
-    };
-    self.addGuest = function() {
-        self.guestListArray.push({guestName: self.guestName(), guestEmail: self.guestEmail()});
-    }
+
 }
+
+
+ko.bindingHandlers.enterkey = {
+    init: function (element, valueAccessor, allBindings, viewModel) {
+        var callback = valueAccessor();
+        $(element).keypress(function (event) {
+            var keyCode = (event.which ? event.which : event.keyCode);
+            if (keyCode === 13) {
+                callback.call(viewModel);
+                return false;
+            }
+            return true;
+        });
+    }
+};
 
     var vm = new MyViewModel();
 
